fa577405fd04ce3338575017026699042bb95e9a
Lakshmi256 <lp268742@gmail.com>
2019-12-30 11:33:27 +0530

    second week

--- com/BridgeLabz/BasicCorePrograms/Harmonic.java
+++
@@ -1,9 +0,0 @@
-package com.BridgeLabz.BasicCorePrograms;
-import com.BridgeLabz.utility.Util;
-public class Harmonic {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int n=Util.readInt();
-Util.harmonic(n);
-	}
-}
--- com/BridgeLabz/LogicalPrograms/TicTacToe.java
+++
@@ -1,9 +0,0 @@
-package com.BridgeLabz.LogicalPrograms;
-import com.BridgeLabz.utility.*;
-public class TicTacToe {
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-char[][] a={{' ',' ',' '},{' ',' ',' ',},{' ',' ',' ',}};
-Util.tictactoe(a);
-}
-}
